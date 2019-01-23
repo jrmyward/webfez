65d6d4352758b991668727e2f35477f6b0d5bdef
Jeremy Ward <jrmy.ward@gmail.com>
2019-01-23 07:09:31 -0800

    Add base layout and static page controller.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def alert_title_for(name)
+    case name
+    when 'alert', 'danger', 'error' then 'Opps...'
+    when 'notice' then 'Success!'
+    else [name.capitalize].join('!')
+    end
+  end
+
+  def alert_type_for(name)
+    case name
+    when 'notice' then 'success'
+    when 'alert', 'error' then 'danger'
+    else name
+    end
+  end
 end
