2cdcac7da00b711949ae7096b227df2a59bdb477
Jeremy Ward <jrmy.ward@gmail.com>
2019-01-26 12:32:58 -0800

    Ensure trailing slash on static page urls.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  before_action :ensure_trailing_slash
+
   def home; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Static pages
+  get '/about', to: 'pages#about'
+  get '/charity', to: 'pages#charity'
+  get '/contact', to: 'pages#contact'
+  get '/events', to: 'pages#events'
+  get '/facility-rental', to: 'pages#facility_rental'
+  get '/membership', to: 'pages#membership'
+  get '/news-updates', to: 'pages#news'
+
   root to: 'pages#home'
 end
