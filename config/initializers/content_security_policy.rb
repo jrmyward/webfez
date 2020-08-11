0b4dc679dbe4ca081de480c8dd2ff8b0f2732d28
Jeremy Ward <jrmy.ward@gmail.com>
2020-08-11 11:34:02 -0700

    Fix GTM content policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https
+    policy.script_src :self, 'https://www.googletagmanager.com', 'https://www.google-analytics.com'
     policy.style_src   :self, :https
   elsif Rails.env.development?
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
